ba6ace94dcf8b5826c34aeafa70ce60a2a761795
Mikael Korpela <mikael@ihminen.org>
2014-12-13 17:30:06 +0200

    Run SemanticWiki DB update at update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,8 +5,11 @@
 
 # Update dependencies
 echo "Updating dependencies..."
-php composer.phar install
+php composer.phar update
 
 # Update Mediawiki
-echo "Running update for Mediawiki"
+echo "Running update script for MediaWiki"
 vagrant ssh -c "cd /var/www/public/wiki/ && php maintenance/update.php"
+
+echo "Running update script for Semantic MediaWiki"
+vagrant ssh -c "cd /var/www/public/wiki/ && php extensions/SemanticMediaWiki/maintenance/SMW_refreshData.php -d 50 -v"
