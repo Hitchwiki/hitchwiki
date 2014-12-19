4430373e4eb8568df63f942de16621a2fcf2ef55
Mikael Korpela <mikael@ihminen.org>
2014-12-19 18:10:55 +0200

    robots.txt

--- public/robots.txt.php
+++ public/robots.txt.php
@@ -1,6 +1,16 @@
-# http://www.robotstxt.org/
-# https://www.mediawiki.org/wiki/Manual:Robots.txt
+<?php
+/* robots.txt
+ * http://www.robotstxt.org/
+ * https://www.mediawiki.org/wiki/Manual:Robots.txt
+ */
 
+header("Content-Type: text/plain");
+
+$hwConfig = parse_ini_file("../configs/settings.ini", true);
+
+// Production environment
+if(isset($hwConfig['general']['env']) && $hwConfig['general']['env'] == 'prod'):
+?>
 # No special pages (edit pages, diff pages, etc)
 User-agent: *
 Disallow: /wiki/
@@ -16,3 +26,12 @@ Disallow: /skins/
 # Allow the Internet Archiver to index action=raw and thereby store the raw wikitext of pages
 User-agent: ia_archiver
 Allow: /*&action=raw
+<?php
+
+// Dev environment
+else:
+?>
+User-agent: *
+Disallow: /
+<?php
+endif;
