214a400b705929e63561fa269906d0b9cae4eae1
Mikael Korpela <mikael@ihminen.org>
2014-12-19 14:28:01 +0200

    Don't run import pages on update, we have a separate tool for it

--- scripts/update.sh
+++ scripts/update.sh
@@ -16,7 +16,8 @@ vagrant ssh -c "cd $VAGRANT_WIKIDIR && php maintenance/update.php --quick --conf
 echo "Running update script for Semantic MediaWiki"
 vagrant ssh -c "cd $VAGRANT_WIKIDIR && php extensions/SemanticMediaWiki/maintenance/SMW_refreshData.php -d 50 -v"
 
-vagrant ssh -c "bash /var/www/scripts/vagrant_import_pages.sh"
+# @TODO: ask if to update?
+#vagrant ssh -c "bash /var/www/scripts/vagrant_import_pages.sh"
 
 echo ""
 echo "All done!"
