a12ef0ba9ad4f505432823c86d8c9f4374062366
Olexandr Melnyk <me@omelnyk.net>
2015-01-13 19:42:50 +0200

    #69: print out relevant mediawiki templates

--- scripts/migratebot/migrate.py
+++ scripts/migratebot/migrate.py
@@ -10,11 +10,11 @@ class CachedHttpRequest:
         hash = hashlib.md5(url).hexdigest()
         filename = cache_dir + '/' + hash
         if (os.path.isfile(filename)):
-            print 'Loading cache for %s from %s' % (url, filename)
+            #print 'Loading cache for %s from %s' % (url, filename)
             with open(filename, 'r') as cache_file:
                 content = cache_file.read().decode('utf-8')
         else:
-            print 'Saving cache for %s into %s' % (url, filename)
+            #print 'Saving cache for %s into %s' % (url, filename)
             content = requests.get(url=url).text
             with open(filename, 'w') as cache_file:
                 cache_file.write(content.encode('utf-8'))
@@ -51,16 +51,7 @@ class GoogleGeocode(object):
         txt = CachedHttpRequest.request(url, self.cache_dir)
         return json.loads(txt)
 
-scoreThreshold = 24 # magic!
-relevanceFilter = [
-    {'fcl': 'L', 'fcode': 'CONT'}, # continent
-    {'fcl': 'S', 'fcode': 'AIRP'}, # airport
-    {'fcl': 'L', 'fcode': 'RGN'}, # region
-    {'fcl': 'T', 'fcode': 'ISL'}, # island
-    {'fcl': 'T', 'fcode': 'ISLS'}, # islands
-    {'fcl': 'A'}, # country or administrative division
-    {'fcl': 'P'} # city or village
-]
+score_threshold = 24 # magic!
 motorway_regex = '[A-Z]?-?\d+\s*(\((\w|\s)+\))'
 border_regex = '.*border (crossing|checkpoint)'
 
@@ -70,59 +61,87 @@ google_geocode = GoogleGeocode('./.cache')
 site = pywikibot.Site()
 gen = pagegenerators.AllpagesPageGenerator(site=site)
 disamb_cat = pywikibot.Category(site, 'Disambiguation')
-disamb_page_titles = [article.title() for article in disamb_cat.articles()]
+disamb_pages = [article.title() for article in disamb_cat.articles()]
 
 count = 0
 for page in gen:
-    if not page.isRedirectPage() and page.title() not in disamb_page_titles:
+    if not page.isRedirectPage() and page.title() not in disamb_pages:
         print '#%d. %s' % (count + 1, page.title().encode('utf-8'))
         print 'http://hitchwiki.org/en/' + page.title(asUrl=True)
 
-        if not re.match(motorway_regex, page.title()) and not re.match(border_regex, page.title()): # no motorway info in GeoNames DB
-            data = geonames.lookup(page.title())
+        entity = None
+        properties = None
 
-            if (
-                data["totalResultsCount"] > 0
-                and data['geonames'][0]['score'] >= scoreThreshold
-                and any(all(item in data['geonames'][0].items() for item in filter.items()) for filter in relevanceFilter)
-            ):
-                print 'GeoNames name: %s' % (data['geonames'][0]['name'].encode('utf-8'))
-                print 'GeoNames Location: %s %s' % (
-                    data['geonames'][0]['lat'],
-                    data['geonames'][0]['lng']
-                )
-                if 'bbox' in data['geonames'][0]:
-                    print 'GeoNames bounding box: %s %s - %s %s' % (
-                        data['geonames'][0]['bbox']['north'],
-                        data['geonames'][0]['bbox']['west'],
-                        data['geonames'][0]['bbox']['south'],
-                        data['geonames'][0]['bbox']['east']
-                    )
-                else:
-                    print 'No bounding box data in GeoNames DB'
-
-                google_data = google_geocode.lookup(page.title())
-                if google_data['results']:
-                    print 'Google Geocode bounding box: %s %s - %s %s' % (
-                        google_data['results'][0]['geometry']['viewport']['northeast']['lat'],
-                        google_data['results'][0]['geometry']['viewport']['southwest']['lng'],
-                        google_data['results'][0]['geometry']['viewport']['southwest']['lat'],
-                        google_data['results'][0]['geometry']['viewport']['northeast']['lng']
-                    )
-                else:
-                    print 'No Google Geocode viewport data'
-            else:
-                print '-'
-        else: # use Google Geocode for motorway and border crossing lookup
+        if re.match(motorway_regex, page.title()): # {Area Type=Road} (no relevant info in GeoNames DB)
             google_data = google_geocode.lookup(page.title())
             if google_data['results']:
-                print 'Google GeoCode bounding box: %s %s - %s %s' % (
-                    google_data['results'][0]['geometry']['viewport']['northeast']['lat'],
-                    google_data['results'][0]['geometry']['viewport']['southwest']['lng'],
-                    google_data['results'][0]['geometry']['viewport']['southwest']['lat'],
-                    google_data['results'][0]['geometry']['viewport']['northeast']['lng']
-                )
+                location = google_data['results'][0]['geometry']['location']
+                viewport = google_data['results'][0]['geometry']['viewport']
+                entity = 'Area'
+                properties = {
+                    'Type': 'Road',
+                    'Location': '%s,%s' % (location['lng'], location['lat']),
+                    'Bbox': "%s,%s,%s,%s" % (viewport['southwest']['lng'], viewport['southwest']['lat'], viewport['northeast']['lng'], viewport['northeast']['lat']),
+                    'Countries': '' # TODO
+                }
+        elif re.match(border_regex, page.title()): # {Area Type=Border crossing} (no relevant info in GeoNames DB)
+            google_data = google_geocode.lookup(page.title())
+            if google_data['results']:
+                location = google_data['results'][0]['geometry']['location']
+                viewport = google_data['results'][0]['geometry']['viewport']
+                entity = 'Area'
+                properties = {
+                    'Type': 'Border crossing',
+                    'Location': '%s,%s' % (location['lng'], location['lat']),
+                    'Bbox': "%s,%s,%s,%s" % (viewport['southwest']['lng'], viewport['southwest']['lat'], viewport['northeast']['lng'], viewport['northeast']['lat']),
+                    'Countries': '' # TODO
+                }
+        else:
+            geonames_data = geonames.lookup(page.title())
+            if (geonames_data["totalResultsCount"] > 0 and geonames_data['geonames'][0]['score'] >= score_threshold):
+                geonames_result = geonames_data['geonames'][0]
 
+                google_data = google_geocode.lookup(page.title())
+
+                properties = {
+                    'Location': '%s,%s' % (geonames_result['lat'], geonames_result['lng'])
+                }
+
+                if google_data['results']:
+                    viewport = google_data['results'][0]['geometry']['viewport']
+                    properties['Bbox'] = '%s,%s,%s,%s' % (viewport['southwest']['lng'], viewport['southwest']['lat'], viewport['northeast']['lng'], viewport['northeast']['lat'])
+                elif 'bbox' in geonames_result: # bbox sucks for countries like France with remote islands, so only use as a fallback strategy
+                    bbox = geonames_result['bbox']
+                    properties['Bbox'] = '%s,%s,%s,%s' % (bbox['west'], bbox['south'], bbox['east'], bbox['north'])
+
+                if geonames_result['fcl'] == 'A' and ('fcode' not in geonames_result or geonames_result['fcode'] in ['PCL', 'PCLI', 'PCLF']): # country
+                    entity = 'Country'
+                    properties.update({ # TODO
+                        'Population': '',
+                        'Capital': '',
+                        'Currency': ''
+                    })
+                else:
+                    properties['Countries'] = '' # TODO
+                    if geonames_result['fcl'] == 'P': # city
+                        entity = 'City'
+                    elif geonames_result['fcl'] == 'L' and geonames_result['fcode'] == 'CONT': # continent
+                        entity = 'Area'
+                        properties['Type'] = 'Continent'
+                    elif geonames_result['fcl'] == 'A' or (geonames_result['fcl'] == 'L' and geonames_result['fcode'] == 'RGN'): # region
+                        entity = 'Area'
+                        properties['Type'] = 'Region'
+                    elif geonames_result['fcl'] == 'T' and geonames_result['fcode'] in ['ISL', 'ISLS']: # island
+                        entity = 'Area'
+                        properties['Type'] = 'Islands'
+                    elif geonames_result['fcl'] == 'S' and geonames_result['fcode'] == 'AIRP': # airport
+                        entity = 'Area'
+                        properties['Type'] = 'Airport'
+
+        if entity:
+            print "{{%s\n|%s\n}}" % (entity, "\n|".join(['%s=%s' % (k, v) for k, v in properties.items()]))
+        else:
+            print '-'
         print
         #page.text = page.text.replace('foo', 'bar')
         #page.save('Replacing "foo" with "bar"')  # Saves the page
