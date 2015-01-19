f7df380083b36f32aceed8c97a0531b2fab30a1c
Mikael Korpela <mikael@ihminen.org>
2015-01-19 12:32:12 +0200

    More verbal update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,35 +6,49 @@ VAGRANT_CONFPATH=/var/www/configs/mediawiki.php
 VAGRANT_WIKIDIR=/var/www/public/wiki
 
 # Update dependencies
+echo ""
 echo "Updating Hitchwiki dependencies..."
 php composer.phar update
 
+echo ""
 echo "Updating MediaWiki dependencies..."
 cd public/wiki
 git pull
 php composer.phar update
 
+echo ""
+echo "Update Vector skin..."
 cd skins/Vector
 git pull
 cd ../../
 
+echo ""
+echo "Update VisualEditor..."
 cd extensions/VisualEditor
 git pull
 git submodule update --init
 
 # Update Mediawiki
+echo ""
 echo "Running update script for MediaWiki"
 vagrant ssh -c "cd $VAGRANT_WIKIDIR && php maintenance/update.php --doshared --quick --conf $VAGRANT_CONFPATH"
 
+echo ""
 echo "Running update script for Semantic MediaWiki"
 vagrant ssh -c "cd $VAGRANT_WIKIDIR && php extensions/SemanticMediaWiki/maintenance/SMW_refreshData.php -d 50 -v"
 
 # Update assets for HWMaps
+echo ""
+echo "Update assets for HWMaps..."
 vagrant ssh -c "cd $VAGRANT_WIKIDIR/extensions/HWMap && bower update --config.interactive=false"
 
+echo ""
+echo "Update assets for HitchwikiVector..."
 # Update assets for HitchwikiVector
 vagrant ssh -c "cd $VAGRANT_WIKIDIR/extensions/HitchwikiVector && bower update --config.interactive=false"
 
+echo ""
+echo "Fetch latest localisation files..."
 # Update locales
 vagrant ssh -c "cd $VAGRANT_WIKIDIR && php extensions/LocalisationUpdate/update.php"
 
