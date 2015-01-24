edd2d0168a8389ffa66287f70f60d5799e46845e
Olexandr Melnyk <me@omelnyk.net>
2015-01-24 16:37:46 +0200

    abstract scripts/update.sh away from vagrant; add vagrant wrapper for it

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,59 +1,68 @@
 #!/bin/bash
 
+#
 # Hitchwiki update script
+#
 
-VAGRANT_CONFPATH=/var/www/configs/mediawiki.php
-VAGRANT_WIKIDIR=/var/www/public/wiki
+if [ ! -f Vagrantfile ]; then # an arbirtrary file that appears only once in the whole repository tree
+    echo "ERROR: Bad working directory ($(pwd))."
+    echo "Scripts have to be run from the root directory of the hitchwiki repository."
+    echo "Aborting."
+    exit 1
+fi
+source "scripts/path_resolve.sh"
 
-# Update dependencies
 echo ""
-echo "Updating Hitchwiki dependencies..."
+
+echo "Update Hitchwiki dependencies..."
 php composer.phar update
-
 echo ""
-echo "Updating MediaWiki dependencies..."
-cd public/wiki
+
+echo "Update MediaWiki dependencies..."
+cd "$WIKIDIR"
 git pull
 php composer.phar update
-
 echo ""
+
 echo "Update Vector skin..."
-cd skins/Vector
+cd "$WIKIDIR/skins/Vector"
 git pull
-cd ../../
-
 echo ""
+
 echo "Update VisualEditor..."
-cd extensions/VisualEditor
+cd "$WIKIDIR/extensions/VisualEditor"
 git pull
 git submodule update --init
-
-# Update Mediawiki
 echo ""
-echo "Running update script for MediaWiki"
-vagrant ssh -c "cd \"$VAGRANT_WIKIDIR\" && php maintenance/update.php --doshared --quick --conf \"$VAGRANT_CONFPATH\""
 
+echo "Run update script for MediaWiki..."
+cd "$WIKIDIR"
+php maintenance/update.php --doshared --quick --conf "$MWCONFFILE"
 echo ""
-echo "Running update script for Semantic MediaWiki"
-vagrant ssh -c "cd \"$VAGRANT_WIKIDIR\" && php extensions/SemanticMediaWiki/maintenance/SMW_refreshData.php -d 50 -v"
 
-# Update assets for HWMaps
+echo "Run update script for Semantic MediaWiki..."
+cd "$WIKIDIR"
+php extensions/SemanticMediaWiki/maintenance/SMW_refreshData.php -d 50 -v
 echo ""
-echo "Update assets for HWMaps..."
-vagrant ssh -c "cd \"$VAGRANT_WIKIDIR/extensions/HWMap\" && bower update --config.interactive=false"
 
+echo "Update assets for HWMap..."
+cd "$WIKIDIR/extensions/HWMap"
+bower update --config.interactive=false
 echo ""
+
 echo "Update assets for HitchwikiVector..."
-# Update assets for HitchwikiVector
-vagrant ssh -c "cd \"$VAGRANT_WIKIDIR/extensions/HitchwikiVector\" && bower update --config.interactive=false"
-
+cd "$WIKIDIR/extensions/HitchwikiVector"
+bower update --config.interactive=false
 echo ""
-echo "Fetch latest localisation files..."
-# Update locales
-vagrant ssh -c "cd \"$VAGRANT_WIKIDIR\" && php extensions/LocalisationUpdate/update.php"
 
-# @TODO: ask if to update?
-#vagrant ssh -c "bash /var/www/scripts/import_pages.sh"
-
+echo "Fetch latest localisation files..." # Update locales
+cd "$WIKIDIR"
+php extensions/LocalisationUpdate/update.php
 echo ""
+
+# @TODO: ask if to import?
+# cd "$SCRIPTDIR"
+# ./scripts/import_pages.sh
+# echo ""
+
 echo "All done!"
---
+++ scripts/vagrant_update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# Hitchwiki update script for Vagrant setup
+#
+
+if [ ! -f Vagrantfile ]; then # an arbirtrary file that appears only once in the whole repository tree
+    echo "ERROR: Bad working directory ($(pwd))."
+    echo "Scripts have to be run from the root directory of the hitchwiki repository."
+    echo "Aborting."
+    exit 1
+fi
+source "scripts/path_resolve.sh"
+
+# Run update script inside Vagrant
+vagrant ssh -c "cd \"$VAGRANT_ROOTDIR\" && \"$VAGRANT_SCRIPTDIR/update.sh\""
