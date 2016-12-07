933492bb067c1c397d1efebd253f383b3dc9dca6
Mikael Korpela <mikael@ihminen.org>
2016-12-07 16:27:20 +0200

    Add clear to re-install script

--- scripts/re_install.sh
+++ scripts/re_install.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+clear
+
 echo "Removing previous files and Vagrant machine..."
 
 rm -fr composer.lock && \
 rm -fr public/wiki && \
 vagrant destroy && \
-./scripts/install.sh
+./scripts/install.sh;
