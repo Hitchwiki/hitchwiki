a0d13139539e378f0f33a197fd2461cac0395075
Olexandr Melnyk <omelnyk@gmail.com>
2016-12-08 16:26:30 +0000

    add scripts/vagrant/{permissions.sh,pull_hw_extensions.sh}

---
+++ scripts/vagrant/permissions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# Shorthand to run scripts/permissions.sh inside the Vagrant box
+#
+# On not Vagrant-based setups (eg. production), directly invoke scripts/permissions.sh instead
+#
+
+if [ ! -f Vagrantfile ]; then # an arbirtrary file that appears only once in the whole repository tree
+    echo "ERROR: Bad working directory ($(pwd))."
+    echo "Scripts have to be run from the root directory of the hitchwiki repository."
+    echo "Aborting."
+    exit 1
+fi
+
+source "scripts/_path_resolve.sh"
+
+# Run permissions script inside Vagrant
+vagrant ssh -c "cd \"$VAGRANT_ROOTDIR\" && bash \"$VAGRANT_SCRIPTDIR/permissions.sh\""
