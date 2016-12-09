77ab154f5130fbaaf4200aa17d337560321e87db
Olexandr Melnyk <omelnyk@gmail.com>
2016-12-09 17:44:16 +0000

    fix folder issue in bot_install

--- scripts/bot/bot_install.sh
+++ scripts/bot/bot_install.sh
@@ -19,7 +19,8 @@ WIKIBOTDIR="$SCRIPTDIR/bot"
 sudo apt-get install python-pip python-dev libmysqlclient-dev
 sudo pip install MySQL-python requests httplib2 ftfy
 
-if cd "$WIKIBOTDIR/.cache"; then
+cd "$WIKIBOTDIR"
+if cd ".cache"; then
     git stash # preserve fresh cache entries
     git pull
     git stash pop
@@ -27,7 +28,8 @@ else
     git clone https://github.com/hitchwiki/hitchwiki-migrate-cache.git .cache
 fi
 
-if cd "$WIKIBOTDIR/pywikibot-core"; then
+cd "$WIKIBOTDIR"
+if cd "pywikibot-core"; then
     git pull
 else
     git clone https://github.com/wikimedia/pywikibot-core.git pywikibot-core
