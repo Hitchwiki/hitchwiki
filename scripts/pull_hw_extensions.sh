6c332b52e387e03ebf3c3cf9eaec8d8805ed90e5
Olexandr Melnyk <omelnyk@gmail.com>
2016-12-12 13:39:10 +0200

    use path_resolve in pull_hw_extensions

--- scripts/pull_hw_extensions.sh
+++ scripts/pull_hw_extensions.sh
@@ -9,54 +9,66 @@
 # extensions, use update.sh (or its wrapper vagrant_update.sh) instead
 #
 
-echo
-echo "One script to pull 'em all..."
-echo
+if [ ! -f Vagrantfile ]; then # an arbirtrary file that appears only once in the whole repository tree
+    echo "ERROR: Bad working directory ($(pwd))."
+    echo "Scripts have to be run from the root directory of the hitchwiki repository."
+    echo "Aborting."
+    exit 1
+fi
 
-cd "$(dirname $0)/.."
+source "scripts/_path_resolve.sh"
+
+echo
+echo "Update Hitchwiki's in-house extensions..."
+echo
 
 # Main repo
+cd "$ROOTDIR"
 git pull origin master
-
-# Do the extensions
-cd extensions
+echo
 
 # HitchwikiVector
-cd HitchwikiVector
+cd "$ROOTDIR/extensions/HitchwikiVector"
 git pull origin master
+echo
 bower update
+echo
 
 # HWMaps
-cd ../HWMap
+cd "$ROOTDIR/extensions/HWMap"
 git pull origin master
+echo
 bower update
+echo
 
 # HWComments
-cd ../HWComments
+cd "$ROOTDIR/extensions/HWComments"
 git pull origin master
+echo
 
 # HWRatings
-cd ../HWRatings
+cd "$ROOTDIR/extensions/HWRatings"
 git pull origin master
+echo
 
 # HWWaitingTime
-cd ../HWWaitingTime
+cd "$ROOTDIR/extensions/HWWaitingTime"
 git pull origin master
-
-
-cd ../../
 echo
+
 echo "Run post-install-cmd for HWMap extension..."
+cd "$ROOTDIR"
 composer run-script post-update-cmd -d ./extensions/HWMap
+
 echo
 echo "Run post-install-cmd for HitchwikiVector extension..."
+cd "$ROOTDIR"
 composer run-script post-update-cmd -d ./extensions/HitchwikiVector
+
 echo
 echo "Run post-install-cmd for HWRatings extension..."
+cd "$ROOTDIR"
 composer run-script post-update-cmd -d ./extensions/HWRatings
-echo
-echo "-------------------------------------------------------------------------"
-
 
 echo
-echo "All done!"
+echo "All in-house extensions updated!"
