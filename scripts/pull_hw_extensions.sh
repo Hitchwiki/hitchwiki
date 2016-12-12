d325b57b4e1c76b34b31f426591720607ecca359
Olexandr Melnyk <omelnyk@gmail.com>
2016-12-12 13:45:38 +0200

    preserve local changes in pull_hw_extensions

--- scripts/pull_hw_extensions.sh
+++ scripts/pull_hw_extensions.sh
@@ -5,6 +5,9 @@
 # pull HitchwikiVector, HWMap, HWComments, HWRatings, HWWaitingTime extensions
 # and update their frontend assets
 #
+# Relies on "git stash" and "git stash pop" to preserve local changes made to
+# extension files
+#
 # For a proper update, including backend dependencies, MediaWiki and third-party
 # extensions, use update.sh (or its wrapper vagrant_update.sh) instead
 #
@@ -23,37 +26,55 @@ echo "Update Hitchwiki's in-house extensions..."
 echo
 
 # Main repo
+echo "Pull main repo..."
 cd "$ROOTDIR"
+git stash
 git pull origin master
+git stash pop
 echo
 
 # HitchwikiVector
+echo "Pull HitchwikiVector repo..."
 cd "$ROOTDIR/extensions/HitchwikiVector"
+git stash
 git pull origin master
+git stash pop
 echo
 bower update
 echo
 
 # HWMaps
+echo "Pull HWMap repo..."
 cd "$ROOTDIR/extensions/HWMap"
+git stash
 git pull origin master
+git stash pop
 echo
 bower update
 echo
 
 # HWComments
+echo "Pull HWComments repo..."
 cd "$ROOTDIR/extensions/HWComments"
+git stash
 git pull origin master
+git stash pop
 echo
 
 # HWRatings
+echo "Pull HWRatings repo..."
 cd "$ROOTDIR/extensions/HWRatings"
+git stash
 git pull origin master
+git stash pop
 echo
 
 # HWWaitingTime
+echo "Pull HWWaitingTime repo..."
 cd "$ROOTDIR/extensions/HWWaitingTime"
+git stash
 git pull origin master
+git stash pop
 echo
 
 echo "Run post-install-cmd for HWMap extension..."
