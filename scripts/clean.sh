fa7f311567cc30bb07d619eb19f2b8ebbbdb55f1
Mikael Korpela <mikael@ihminen.org>
2017-10-11 16:21:55 +0300

    Extend cleanup script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -8,7 +8,14 @@ set -e
 
 source "scripts/_path_resolve.sh"
 
-echo "$WIKIDIR"
+# Remove Mediawiki folder
 rm -rf "$WIKIDIR"
-#TODO: remove mysql maybe?
-#TODO: undo crazy sed stuff... Map/Map.php
+
+# Other folders or files created by installation
+rm "$ROOTDIR/composer.lock"
+rm -fr "$ROOTDIR/html"
+rm -fr "$ROOTDIR/public/composer"
+
+# Remove log files
+rm "$ROOTDIR/scripts/*-cloudimg-console.log"
+rm "$ROOTDIR/*-cloudimg-console.log"
