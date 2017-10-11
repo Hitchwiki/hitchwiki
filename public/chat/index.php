2c0e0a04e87faa2bab031fe37bfc38bfe33489e4
Mikael Korpela <mikael@ihminen.org>
2017-10-11 18:33:01 +0300

    Add IRC chat webclient

---
+++ public/chat/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <title>Hitchwiki chat</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=EDGE" />
+  <link rel="icon" href="/favicon.png">
+  <link rel="copyright" href="https://creativecommons.org/licenses/by-sa/4.0/">
+  <style>
+  html, body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
+  #chat {
+    display: block;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    border: 0;
+    outline: none;
+  }
+  </style>
+</head>
+<body>
+
+  <iframe src="https://webchat.freenode.net?channels=hitchhiking&prompt=1&uio=Mz1mYWxzZSYxMT00MQ69" frameborder="0" id="chat"></iframe>
+
+</body>
+</html>
