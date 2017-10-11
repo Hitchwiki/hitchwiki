75e06df966091ac0452b8bb1f8ef5bee96811cf0
Mikael Korpela <mikael@ihminen.org>
2017-10-11 16:37:21 +0300

    Update cleanup script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -8,14 +8,22 @@ set -e
 
 source "scripts/_path_resolve.sh"
 
+echo ""
+echo "Cleaning up files and folders created by Hitchwiki install..."
+
 # Remove Mediawiki folder
-rm -rf "$WIKIDIR"
+rm -fr "$WIKIDIR"
 
 # Other folders or files created by installation
-rm "$ROOTDIR/composer.lock"
+rm -f "$ROOTDIR/composer.lock"
 rm -fr "$ROOTDIR/html"
 rm -fr "$ROOTDIR/public/composer"
 
 # Remove log files
-rm "$ROOTDIR/scripts/*-cloudimg-console.log"
-rm "$ROOTDIR/*-cloudimg-console.log"
+rm -f "$ROOTDIR/*-cloudimg-console.log"
+rm -f "$ROOTDIR/scripts/*-cloudimg-console.log"
+
+echo "Done! Note that this did not remove config files:"
+echo "- ./configs/settings.ini"
+echo "- ./configs/vagrant.yaml"
+echo ""
