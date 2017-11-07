cf6c29330ba86766d9f724f0d86814fedb37db86
Traumschule Riebau <traumschule@riseup.net>
2017-11-07 17:44:22 +0100

    ansible: skip authorized_keys when none are found

--- scripts/stop_all.sh
+++ scripts/stop_all.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-services="monit apache2 mysql parsoid"
+services="monit apache2 mysqld parsoid"
 if [ $(whoami) != 'root' ]; then echo "I need more rights! (give me names and I make it stop, else: $services)"; exit 1; fi
 if [ $1 ]; then services="$@" ; fi
 echo "Database backup"
 backupninja
 for service in $services; do
+  bin=$(which $service)
   echo "Stopping $service"
-  service $service stop
-  ps ax|grep $service
+  service $service stop || ([ -f /etc/init.d/$service ] && /etc/init.d/$service stop)
+  remaining=$(ps ax|grep $bin|wc -l)
+  if [ $remaining -gt 1 ]
+  then echo "$service is still running, please stop it manually."; ps ax|grep $bin; exit 1
+  fi
 done
 apache2ctl stop
 ps ax|grep apache2
