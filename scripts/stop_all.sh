8ab33232441a68aa891dbe5c14e0c8c7cc7167fb
Traumschule Riebau <traumschule@riseup.net>
2017-11-09 23:56:29 +0100

    ansible: start services after setup; fix stop_all.sh

--- scripts/stop_all.sh
+++ scripts/stop_all.sh
@@ -7,14 +7,13 @@ echo "Database backup"
 backupninja
 #echo "Drop databases"
 #mysql -uroot -proot hitchwiki_en -e "DROP DATABASE hitchwiki_en" # TODO
+apache2ctl stop
 for service in $services; do
-  bin=$(which $service)
   echo "Stopping $service"
   service $service stop || ([ -f /etc/init.d/$service ] && /etc/init.d/$service stop)
+  bin=$(which $service||echo $service)
   remaining=$(ps ax|grep $bin|wc -l)
   if [ $remaining -gt 1 ]
   then echo "$service is still running, please stop it manually."; ps ax|grep $bin; exit 1
   fi
 done
-apache2ctl stop
-ps ax|grep apache2
