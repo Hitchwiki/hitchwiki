38c28f1f8943903a560f7b7b3ea32808248e6105
Traumschule Riebau <traumschule@riseup.net>
2017-11-12 03:12:37 +0100

    ansible: setup_hitchwiki.sh now checks that deploy added localhost to inventory

--- scripts/setup_hitchwiki.sh
+++ scripts/setup_hitchwiki.sh
@@ -2,4 +2,6 @@
 # To be executed via 'ssh -t' or locally
 set -e
 cd $(dirname $0)/ansible
-ansible-playbook hitchwiki.yml --limit="localhost" --user=hitchwiki
+if [ $(grep localhost hosts |wc -l) == 0 ]
+then echo "Please provision/deploy first."; exit 1; fi
+ansible-playbook hitchwiki.yml --limit="localhost"
