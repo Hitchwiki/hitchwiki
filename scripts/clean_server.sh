77197789a55f711a472498300bfcb9ee77f06e79
Traumschule Riebau <traumschule@riseup.net>
2017-11-18 18:53:35 +0100

    disable maildev for now

--- scripts/clean_server.sh
+++ scripts/clean_server.sh
@@ -2,13 +2,16 @@
 USER=hitchwiki
 APT="python-simplejson git python-pip unattended-upgrades composer npm tmux python-pip vim monit curl git unzip zip imagemagick build-essential python-software-properties fail2ban htop backupninja ack-grep nano emacs24-nox nullmailer w3m php-apcu zsh apache2 phpmyadmin php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-gd php7.0-intl php7.0-imap php7.0-mcrypt php7.0-pspell php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xmlrpc php7.0-xsl php7.0-mbstring php7.0-opcache php-memcache php-pear php-imagick php-apcu php-gettext php-xml php-json nodejs mariadb-server software-properties-common python-mysqldb apt-transport-https parsoid"
 PATHS="/var/www /home/$USER /etc/{ansible,monit,letsencrypt,apache2,mediawiki} /etc/init.d/{maildev,parsoid}"
-if [[ $(whoami) != "root" ]]; then echo "Please run this as root.
-I will
+if [[ $(whoami) != "root" ]]; then echo "Please run this as root."; exit 1; fi
+echo "This will:
 - run './scripts/stop_all.sh'
 - purge following packages: $APT
 - remove following paths: $PATHS
 - remove user $USER
-Be sure that this is your intention."; exit; fi
+
+Press <ENTER> to contiue or CTRL+C to abort."
+read
+set -e
 cd $(dirname $0)/..
 ./scripts/stop_all.sh || echo "Failed to stop all services."
 echo "Purging $APT"
--- scripts/deploy_remote.sh
+++ scripts/deploy_remote.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # executed on control machine
 starttime=$(date +%s)
-set -ev
+set -e
 if [ ! -d $(dirname $0)/ansible ]; then echo "Directory ansible not found. Something is wrong. Exiting."; exit 1; fi
 if [ ! $1 ]; then echo -e "Usage: $(basename $0) HOST [PORT]\nSpecify one host as parameter. To deploy multiple hosts see https://github.com/Hitchwiki/hitchwiki/blob/master/INSTALL.md"; exit; fi
 cd $(dirname $0)/ansible
 HOST=$1
 PORT=$2
 REPO=https://github.com/traumschule/hitchwiki
-VERSION=ansible
+VERSION=testing
 WEBROOT=/var/www
 DEPS="git python-pip openssh-client sudo at tmux"
 PULL_ARGS="-U $REPO -C $VERSION -d $WEBROOT -i /etc/ansible/hosts scripts/ansible/hitchwiki.yml"
@@ -26,13 +26,15 @@ then echo "Running 'ssh-keygen' first."; ssh-keygen; fi
 rsync ~/.ssh/id_rsa.pub root@$HOST:.ssh/authorized_keys
 
 echo "Starting deployment to $HOST"
-ssh -t root@$HOST "apt-get update -qq && apt-get install -y $DEPS
+ssh -t root@$HOST "set -ev #apt-get update &&
+  apt-get install -y $DEPS
   [ -d /etc/ansible ] || mkdir /etc/ansible
   echo 'localhost ansible_connection=local' > /etc/ansible/hosts
-  tmux -c 'pip install -U pip && pip install ansible &&
-  ANSIBLE_FORCE_COLOR=1 ansible-pull $PULL_ARGS &&
-  curl -s localhost'"
+  pip install -U pip
+  pip install ansible
+  tmux -c 'ANSIBLE_FORCE_COLOR=1 ansible-pull $PULL_ARGS'
+  curl -s localhost"
 endtime=$(date +%s)
-min=$((endtime-starttime) / 60)))
-sec=$((endtime-starttime) % 60)))
+min=$((endtime-starttime) / 60))
+sec=$((endtime-starttime) % 60))
 echo "Installed Hitchwiki in $min minutes and $sec seconds."
